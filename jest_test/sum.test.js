07aa57df7ed4d4314af7a56ea957b2318763f28d
tsuifei <tsuifei@gmail.com>
2020-03-11 23:03:04 +0100

    add jest test

--- jest_test/sum.test.js
+++ jest_test/sum.test.js
@@ -1,5 +1,11 @@
-const sum = request('./sum');
+const sum = require('./sum');
 
-test('add 1 +2 to equal 3', ()=>{
-  expect(sum(1,2)).toBe(3)
-})
+describe('測試sum', function() {
+  test('add 1 + 2 to equal 3', () => {
+    expect(sum(1, 2)).toBe(3);
+  });
+
+  test('add 42 + 24 to equal 66', () => {
+    expect(sum(42, 24)).toBe(66);
+  });
+});
